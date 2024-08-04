841cd9876ed41c93f3db5008dab1b70a180c3adb
jamesrochabrun <jamesrochabrun@gmail.com>
2024-08-03 22:18:55 -0700

    Initial Commit

---
+++ OpenAIBuildHours/OpenAIBuildHoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAIBuildHoursApp.swift
+//  OpenAIBuildHours
+//
+//  Created by James Rochabrun on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenAIBuildHoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
